00838a3619bb2509a5e2d81d30181bcdda1f16ef
Marques Avery <averymarques4@gmail.com>
2020-08-07 17:59:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |dwarve ,i|
     binding.pry
-    puts "#{i}." dwarve
+    puts i dwarve
   end
 end
 
