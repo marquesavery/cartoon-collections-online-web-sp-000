36271725c38e28b3e1b07f66dfa74498c7b294b4
Marques Avery <averymarques4@gmail.com>
2020-08-07 18:41:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  binding.pry
   if array.include?(cheese_types)
     array
   else
