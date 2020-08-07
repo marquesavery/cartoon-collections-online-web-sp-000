d46389459ff26682e1931fcf46abc784d482dc89
Marques Avery <averymarques4@gmail.com>
2020-08-07 18:14:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,8 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |element|
+    element.upcase
 end
 
 def long_planeteer_calls# code an argument here
